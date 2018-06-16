ce1c1bb1cd5ad5ad3206564f6a9ddac9926b042b
chiranjev <koulsahil04321@gmail.com>
2018-06-16 12:20:47 +0530

    16/6/18

---
+++ bfs.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define mod 1000000007
+
+using namespace std;
+
+void addEdge(vector<int> adj[],int u,int v){
+	adj[u].push_back(v);
+}
+
+void bfs(vector<int> adj[],int V,int u){
+
+	vector<bool> visited(V,false);
+	queue<int> q;
+
+	visited[u]=true;
+	q.push(u);
+
+	while(!q.empty()){
+		u=q.front();
+		q.pop();
+		cout << u << " -> ";
+		for(int i=0;i<adj[u].size();i++){
+			if(!visited[adj[u][i]]){
+				visited[adj[u][i]]=true;
+				q.push(adj[u][i]);
+			}
+		}
+	}
+
+}
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	    // for getting input from input.txt
+	    freopen("input.txt", "r", stdin);
+	    // for writing output to output.txt
+	    freopen("output.txt", "w", stdout);
+	#endif
+
+	int v=5;
+	vector<int> adj[v];
+	addEdge(adj,0,1);
+	addEdge(adj,0,4);
+	addEdge(adj,4,3);
+	addEdge(adj,1,4);
+	addEdge(adj,1,2);
+	addEdge(adj,2,3);
+	addEdge(adj,1,3);
+
+	bfs(adj,v,2);
+
+	return 0;
+}
